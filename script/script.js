34a61713a89a9bbed0664e132e9c4819ae689fa6
Sajmul <sajmul1427@gmail.com>
2024-09-29 21:50:19 +0600

    sd

--- script/script.js
+++ script/script.js
@@ -1,152 +1,79 @@
-// import { data } from "./data.js";
-// let index = 0;
 
+const page = document.querySelector(".page");
+const startQuiz = document.getElementById("startQuiz");
+const exitBtn = document.getElementById("exit");
+const continueBtn = document.getElementById("continue");
+const quesBody = document.getElementById("quesBody");
+const beforeQues = document.querySelector(".question-list");
+const quesNoEl = document.getElementById("quesNO");
+const totalQues = document.getElementById("total-q");
+const nextQuizBtn = document.getElementById("next-quiz");
+const resultBtn = document.getElementById("resultBtn");
 
-// function init() {
-//     const question = data();
+let nextEl;
+let index = 0;
+let correctAns = 0;
+console.log(correctAns);
 
-//     showQues(question);
-    
+startQuiz.addEventListener("click", function () {
+  nextEl = page.nextElementSibling;
+  page.classList.add("hidden");
+  nextEl.classList.remove("hidden");
+});
 
+exitBtn.addEventListener("click", function () {
+  page.classList.remove("hidden");
+  page.nextElementSibling.classList.add("hidden");
+});
 
-//     const nextBtn = document.getElementById('next-quiz');
-    
-//     nextBtn.addEventListener('click', () => {
-//         if (index < question.length) {
-//             nextQues(question);
-//         } else {
-//             showResult();
-//         }
-//     })
-// }
-
-// function showQues(data) {
-//     const ques = document.querySelector('.question-body h3');
-//     const option = document.querySelector('.question-list ul');
-//     const quesNO = document.getElementById('quesNO');
-//     const totalQues = document.getElementById('total-q');
-
-//     if(index < data.length) {
-
-//         quesNO.innerText = index + 1;
-//         totalQues.innerText = data.length;
-
-//         ques.innerHTML = `${index+1}. ${data[index].question}`;
-
-//         data[index].option.forEach((value,i) => {
-//             const li = document.createElement('li');
-//             li.classList.add('list-style');
-//             li.innerText = value;
-
-//             option.appendChild(li);
-//         });
-        
-//     } else {
-//         showResult(data);
-//     }
-// } 
-
-// function nextQues(data) {
-//     const option = document.querySelector('.question-list ul');
-//     while (option.firstChild) {
-//         option.removeChild(option.firstChild);
-//     }
-
-//     index += 1;
-//     showQues(data);
-
-//     if(index === (data.length - 1)) {
-//         const nextBtn = document.getElementById('next-quiz');
-//         nextBtn.innerText = 'Show Result';
-//     }
-    
-// }
-
-// function showResult() {
-//     const nextBtn = document.getElementById('next-quiz');
-    
-// }
-
-// init();
-
-
-
-
-// const question = data();
-// console.log(question);
-
-let index = 0 ;
-
-
-const page = document.querySelector('.page');
-const startQuiz = document.getElementById('startQuiz');
-const exitBtn = document.getElementById('exit');
-const continueBtn = document.getElementById('continue');
-const quesBody = document.getElementById('quesBody');
-const beforeQues = document.querySelector('.question-list');
-const quesNoEl = document.getElementById('quesNO');
-const totalQues = document.getElementById('total-q');
-const nextQuizBtn = document.getElementById('next-quiz');
-const resultBtn = document.getElementById('resultBtn');
-
-
-let nextEl ; 
-
-startQuiz.addEventListener('click', function() {
-    nextEl = page.nextElementSibling;
-    page.classList.add('hidden');
-    nextEl.classList.remove('hidden');
-})
-
-
-exitBtn.addEventListener('click',function() {
-    page.classList.remove('hidden');
-    page.nextElementSibling.classList.add('hidden');
-})
-
-
-continueBtn.addEventListener('click', function() {
-    
-    nextEl.classList.add('hidden');
-    nextEl.nextElementSibling.classList.remove('hidden');
-    showQues();
-    totalQues.innerText = question.length;
-})
-
+continueBtn.addEventListener("click", function () {
+  nextEl.classList.add("hidden");
+  nextEl.nextElementSibling.classList.remove("hidden");
+  showQues();
+  totalQues.innerText = question.length;
+});
 
 function showQues() {
-    const h3 = document.createElement('h3');
-    h3.innerText = `${question[index].no}. ${question[index].question}`;
-    quesBody.appendChild(h3);
-    const ul = document.createElement('ul');
-    ul.classList.add('flex','flex-col','gap-3','mt-6');
+  const h3 = document.createElement("h3");
+  h3.innerText = `${question[index].no}. ${question[index].question}`;
+  quesBody.appendChild(h3);
+  const ul = document.createElement("ul");
+  ul.classList.add("flex", "flex-col", "gap-3", "mt-6");
+
+  question[index].option.forEach((element) => {
+    const li = document.createElement("li");
+    li.classList.add("list-style", "cursor-pointer");
+    li.innerText = element;
+    ul.appendChild(li);
+  });
+
+  quesBody.appendChild(ul);
+  quesNoEl.innerText = question[index].no;
 
 
-    question[index].option.forEach(element => {
-        const li = document.createElement('li');
-        li.classList.add('list-style','cursor-pointer');
-        li.innerText = element;
-        ul.appendChild(li);
+  console.log(e.target.innerText === question[index].ans);
+ 
+  
+  ul.addEventListener("click", function (e) {
+    ul.childNodes.forEach(elem => {
+        elem.classList.remove('bg-green-400', 'border-green-600');
     });
-
-    quesBody.appendChild(ul);
-    quesNoEl.innerText = question[index].no;
-
-    ul.addEventListener('click', function(e) {
-        e.target.classList.add('bg-green-400', 'border-green-600');
-    })
-
-    index++;
-
-    if(index === question.length) {
-        nextQuizBtn.classList.add('hidden');
-        resultBtn.classList.remove('hidden');
+    e.target.classList.add("bg-green-400", "border-green-600");
+    console.log(question[index].ans);
+    if(e.target.innerText === question[index].ans) {
+        correctAns++;
     }
+  });
+  
 
+  if (index === question.length) {
+    nextQuizBtn.classList.add("hidden");
+    resultBtn.classList.remove("hidden");
+  }
 }
 
-nextQuizBtn.addEventListener('click', function() {
-    quesBody.innerHTML = '';
-    showQues();
-})
-
+nextQuizBtn.addEventListener("click", function () {
+  quesBody.innerHTML = "";
+  index++;
+  showQues();
+});
